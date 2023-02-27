ecbe6393ca4d94d4c8feea530e9cf5e645b947dd
Aleksandar Radulovic <aleksandar.radulovic@sap.com>
2023-02-27 15:52:10 +0100

    Web layer + validation

--- src/main/java/com/polarbookshop/catalog/domain/Book.java
+++ src/main/java/com/polarbookshop/catalog/domain/Book.java
@@ -1,6 +1,29 @@
 package com.polarbookshop.catalog.domain;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 
-public record Book(String isbn, String title, String author, BigDecimal price) {
+public record Book(
+        @NotBlank(
+                message = "The book ISBN must be defined")
+        @Pattern(
+                regexp = "^([0-9]{10}|[0-9]{13})$",
+                message = "Invalid ISBN format"
+        )
+        String isbn,
+        @NotBlank(
+                message = "The book title must be defined")
+        String title,
+        @NotBlank(
+                message = "The book author must be defined")
+        String author,
+        @NotNull(
+                message = "The book price must be defined")
+        @Positive(
+                message = "The price cannot be negative value."
+        )
+        BigDecimal price) {
 }
---
+++ src/main/java/com/polarbookshop/catalog/web/BookControllerAdvice.java
@@ -0,0 +1,40 @@
+package com.polarbookshop.catalog.web;
+
+import com.polarbookshop.catalog.domain.BookAlreadyExistsException;
+import com.polarbookshop.catalog.domain.BookNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class BookControllerAdvice {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String bookNotFoundHandler(BookNotFoundException exception) {
+        return exception.getMessage();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    String bookAlreadyExistsHandler(BookAlreadyExistsException exception) {
+        return exception.getMessage();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+        var errors = new HashMap<String, String>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+}
