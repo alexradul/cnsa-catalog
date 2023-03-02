0017f8595b09b36442856a2b5f5edab7eb4457d8
Aleksandar Radulovic <aleksandar.radulovic@gmail.com>
2023-03-02 21:23:10 +0100

    Implementing persistence aspect
    This commit includes several updates:
    * adding Spring Data JDBC + PostgreSQL driver to the project
    * adding testcontainers for integration test
    * introducing 'integration' profile for tests
    * switching to Spring Data repository
    * enabling Spring Data auditing feature to add auditing information to the Book entity

---
+++ src/main/java/com/polarbookshop/catalog/demo/BookDataLoader.java
@@ -0,0 +1,20 @@
+package com.polarbookshop.catalog.demo;
+
+import com.polarbookshop.catalog.domain.Book;
+import com.polarbookshop.catalog.domain.BookRepository;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(name = "catalog-service.setupTestData", havingValue = "true")
+public class BookDataLoader {
+    @Bean
+    ApplicationRunner loadBookTestData(BookRepository repository) {
+        return args -> {
+            repository.save(Book.of("1234", "The Old Man and the Sea", "Ernest Hemingway", 100.0));
+            repository.save(Book.of("4321", "Catcher in the Ray", "J.D. Salinger", 100.0));
+        };
+    }
+}
