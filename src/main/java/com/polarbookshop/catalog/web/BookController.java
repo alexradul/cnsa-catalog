87476c5a363b41cfc4ec80ac26f9f65e6f787340
Aleksandar Radulovic <aleksandar.radulovic@gmail.com>
2023-04-04 23:11:34 +0200

    Adding some logging statements

--- src/main/java/com/polarbookshop/catalog/web/BookController.java
+++ src/main/java/com/polarbookshop/catalog/web/BookController.java
@@ -3,12 +3,14 @@ package com.polarbookshop.catalog.web;
 import com.polarbookshop.catalog.domain.Book;
 import com.polarbookshop.catalog.domain.BookService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
+@Slf4j
 @RequestMapping("books")
 @RequiredArgsConstructor
 public class BookController {
@@ -16,28 +18,33 @@ public class BookController {
 
     @GetMapping
     public Iterable<Book> books() {
+        log.info("Fetching all books from the catalog");
         return bookService.viewBookList();
     }
 
     @GetMapping("{isbn}")
     public Book getByIsbn(@PathVariable String isbn) {
+        log.info("Fetching book having ISBN {} from catalog", isbn);
         return bookService.viewDetails(isbn);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book post(@Valid @RequestBody Book book) {
+        log.info("Adding book {} {} by {} to catalog", book.isbn(), book.title(), book.author());
         return bookService.addBookToCatalog(book);
     }
 
     @DeleteMapping("{isbn}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable String isbn) {
+        log.info("Deleting book with ISBN {} from catalog", isbn);
         bookService.removeBookFromCatalog(isbn);
     }
 
     @PutMapping("{isbn}")
     public Book put(@PathVariable String isbn,@Valid @RequestBody Book book) {
+        log.info("Updating book with ISBN {}", isbn);
         return bookService.editDetails(isbn, book);
     }
 }
